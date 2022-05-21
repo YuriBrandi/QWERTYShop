05e454ddebae6d7754b09c3aca122f7a9bae1130
Yuri Brandi <destroyercrazy0@gmail.com>
2022-05-21 22:33:23 +0200

    Testing: numero accessi

--- src/main/java/com/rocca_brandi/progettotsw/testDB.java
+++ src/main/java/com/rocca_brandi/progettotsw/testDB.java
@@ -1,7 +1,7 @@
 package com.rocca_brandi.progettotsw;
 
 import java.io.*;
-import java.sql.SQLException;
+import java.sql.*;
 
 import Model.ConPool;
 import jakarta.servlet.http.*;
@@ -16,19 +16,55 @@ public class testDB extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
+        Connection conn = null;
         try {
-            ConPool.getConnection();
+            conn = ConPool.getConnection();
             out.println("Connessione riuscita");
         } catch (SQLException e) {
             e.printStackTrace();
             out.println("Eccezione in connessione: " + e.getMessage());
         }
 
-
+        if(conn != null){
+            int val = getValore();
+            val++;
+            out.println("<br>Numero accessi: " + val);
+            doUpdate(val);
+        }
 
         out.println("</body></html>");
     }
 
     public void destroy() {
+    }
+
+    public int getValore() {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps =
+                    con.prepareStatement("SELECT prova FROM test");
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+            return -1;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void doUpdate(int value) {
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement ps = con.prepareStatement(
+                    "UPDATE test SET prova = ?",
+                    Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, value);
+
+            if (ps.executeUpdate() != 1) {
+                throw new RuntimeException("UPDATE error.");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
