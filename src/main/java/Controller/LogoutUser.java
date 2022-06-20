19a050e7cd3cb2295d4005e260440d2bcd513e72
Yuri Brandi <destroyercrazy0@gmail.com>
2022-06-20 23:41:48 +0200

    Logout Servlet

--- src/main/java/Controller/LogoutUser.java
+++ src/main/java/Controller/LogoutUser.java
@@ -1,4 +1,20 @@
 package Controller;
 
-public class LogoutUser {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/logout-user")
+public class LogoutUser extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getSession().removeAttribute("utente");
+
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
 }
