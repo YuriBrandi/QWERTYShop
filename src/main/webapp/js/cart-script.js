b930b9e86e7062830f735254d9a594b9aacb0d54
roberto <roberto@dellarocca.org>
2022-06-20 11:56:20 +0200

    cart-script.js (funzionante dato che ho dovuto cambiare la classe di quantity-field a quantity_fld)

--- src/main/webapp/js/cart-script.js
+++ src/main/webapp/js/cart-script.js
@@ -1,14 +1,14 @@
 $(document).ready(function () {
 
     $(".add-btn").click(function(){
-        var value = $(this).siblings(".input-txt_fld.quantity-field").val();
-        $(this).siblings(".input-txt_fld.quantity-field").val(Number(value) + 1);
+        var value = $(this).siblings(".input-txt_fld.quantity_fld").val();
+        $(this).siblings(".input-txt_fld.quantity_fld").val(Number(value) + 1);
     });
 
     $(".remove-btn").click(function(){
-        var value = $(this).siblings(".input-txt_fld.quantity-field").val();
+        var value = $(this).siblings(".input-txt_fld.quantity_fld").val();
         if (value > 1)
-            $(this).siblings(".input-txt_fld.quantity-field").val(Number(value) - 1);
+            $(this).siblings(".input-txt_fld.quantity_fld").val(Number(value) - 1);
     });
 
 });
