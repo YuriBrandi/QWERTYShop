af2b4f2e77dc0dc6d43874ad613f4e402c96f312
Yuri Brandi <destroyercrazy0@gmail.com>
2022-06-25 18:30:05 +0200

    Addedd ContenutoCarrello model

---
+++ src/main/java/Model/ContenutoCarrello.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class ContenutoCarrello {
+    private String email;
+    private int idProdotto;
+    private short quantita;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdProdotto() {
+        return idProdotto;
+    }
+
+    public void setIdProdotto(int idProdotto) {
+        this.idProdotto = idProdotto;
+    }
+
+    public short getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(short quantita) {
+        this.quantita = quantita;
+    }
+}
